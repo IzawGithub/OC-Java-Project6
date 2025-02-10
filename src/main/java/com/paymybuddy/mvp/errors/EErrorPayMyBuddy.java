3940ddd774872a4be4e25edc2b7b30ab49c0b71b
IzawGithub <IzawGithub@pm.me>
2025-02-10 15:00:39 +0100

    Create sealed error class

---
+++ src/main/java/com/paymybuddy/mvp/errors/EErrorPayMyBuddy.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.mvp.errors;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties({"cause", "localizedMessage", "message", "stackTrace", "suppressed"})
+public sealed class EErrorPayMyBuddy extends Exception {}
